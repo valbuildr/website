1bae1a6ab95028eac7c8551713d5ad20bd605a85
valentine <v4lbuild3r@gmail.com>
2024-05-06 10:04:57 -0500

    move to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://valbuilds.gay',
+  output: "server",
+  adapter: netlify()
 });
